9f8b3b60a43c7ce97e5d7aecd31c88873db823da
Alex <alexandremelano@yahoo.com>
2018-05-24 11:58:49 -0400

    first commit

---
+++ currency.js
@@ -0,0 +1,9 @@
+/* eslint "no-console: 0" eslint "spaced-comment: 0 */
+
+const accounting = require('accounting');
+
+/* create a variable and assign it a numeric value*/
+const amount = 98.3456;
+
+/* display the amount */
+console.log(accounting.formatMoney(amount));
